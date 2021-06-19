1c8164a312515a45303173003593b747d52856d3
Vamshidhar <dev.vamshidhar@gmail.com>
2021-06-19 07:47:41 +0000

    edited strong_num.c to use nested loop

---
+++ strong_num.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int n, sum, i, i_copy;
+	int fact_arr[] = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
+	printf("Enter the last Number: ");
+	scanf("%i", &n);
+	for(int i = 1; i <= n; i++){
+	        sum = 0;
+			i_copy = i;
+			while(i_copy){
+				sum += fact_arr[i_copy % 10];
+				i_copy /= 10;
+			}
+			if(sum == i)printf("%i\n", i);
+	}
+	return 0;
+}
