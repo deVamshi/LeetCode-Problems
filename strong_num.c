38e5dfd78d9c785d681e7b1c433f2dcf56dc68ea
Vamshidhar <dev.vamshidhar@gmail.com>
2021-06-19 13:19:59 +0530

    Update strong_num.c

--- strong_num.c
+++ strong_num.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    int n, sum, i, i_copy;
-	int fact_arr[] = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
+    	int n, sum, i, i_copy;
+     	int fact_arr[] = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
 	printf("Enter the last Number: ");
 	scanf("%i", &n);
 	for(int i = 1; i <= n; i++){
